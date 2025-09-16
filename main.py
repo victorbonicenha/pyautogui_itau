3c0c2c7620e648b88fd1073957acb4ae93ccde5c
Victor Bonicenha <141092915+victorbonicenha@users.noreply.github.com>
2025-09-16 08:23:36 -0300

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from extracao_pyautogui import AutoClique
+from time import sleep
+import subprocess
 
 if __name__ == "__main__":
+    # 1. abre o aplicativo do Itaú direto (sem clicar no menu do Windows)
+    caminho_app = r"C:\Users\tom\AppData\Local\Aplicativo Itau\itauaplicativo.exe"
+    subprocess.Popen([caminho_app])
+
+    sleep(20)
     bot = AutoClique(tempo_padrao=1)
 
-    # 1. abre o app
-    bot.clicar_imagem("icone_itau_vm.png", confianca=0.8, double=True)
-
-
     # 2. clica no campo agência e digita
-    bot.clicar_e_digitar("itau_agencia.png", "7243", confianca=0.8)
+    bot.clicar_e_digitar("itau_agencia_tentativa.PNG", "7243", confianca=0.5)
 
     # 3. clica no campo conta e digita
-    bot.clicar_e_digitar("conta_itau.png", "40645-6", confianca=0.8)
+    bot.clicar_e_digitar("conta_itau.png", "40645-6", confianca=0.5)
 
     # 4. clica no botão acessar
-    bot.clicar_imagem("botao_login_itau.png", confianca=0.8)
+    bot.clicar_imagem("botao_login_itau.png", confianca=0.5)
 
     print("Fluxo finalizado")
